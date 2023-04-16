1933cfc73b1d4579dc58c07f0e728881e564b2cf
Dubrovsky18 <dubrovin.02@list.ru>
2023-04-16 05:23:45 +0300

    Web-Site cookie with FoldersTests V:2.1

--- FileLoad/FileDownload.go
+++ FileLoad/FileDownload.go
@@ -2,6 +2,7 @@ package FileLoad
 
 import (
 	"fmt"
+	_ "github.com/gorilla/sessions"
 	"html/template"
 	"io"
 	"net/http"
@@ -10,6 +11,14 @@ import (
 )
 
 func HandleDownload(w http.ResponseWriter, r *http.Request) {
+
+	// Проверяем, залогинен ли пользователь
+	session, err := r.Cookie("session")
+	if err != nil || session.Value == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
 	t, err := template.ParseFiles("templates/download.html", "templates/header.html", "templates/footer.html")
 	if err != nil {
 		fmt.Fprintf(w, err.Error())
@@ -17,12 +26,20 @@ func HandleDownload(w http.ResponseWriter, r *http.Request) {
 
 	t.ExecuteTemplate(w, "download", nil)
 
+	fmt.Fprintf(w, session.Value, "Download page")
 }
 
 func FileDownload(w http.ResponseWriter, r *http.Request) {
 
+	session, err := r.Cookie("session")
+	if err != nil || session.Value == "" {
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+		return
+	}
+
+	fmt.Print(session.Value)
 	// Получаем путь к файлу из параметра запроса
-	filePath := "FileLoad/FilesWebSiteOut/Whale-52.txt"
+	filePath := fmt.Sprintf("FileLoad/FilesWebSiteOut/tests/%s/%s.mp4", session.Value, "OutFile")
 
 	// Открываем файл и проверяем на ошибки
 	file, err := os.Open(filePath)
@@ -52,35 +69,3 @@ func FileDownload(w http.ResponseWriter, r *http.Request) {
 	}
 
 }
-
-//func FileDownload(w http.ResponseWriter, r *http.Request) {
-//	// Путь к файлу, который нужно вернуть
-//	filePath := fmt.Sprintf("FileWebSiteOut/%s", "Whale-52.txt")
-//
-//	// Открываем файл и проверяем на ошибки
-//	file, err := os.Open(filePath)
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
-//	defer file.Close()
-//
-//	// Получаем информацию о файле
-//	fileInfo, err := file.Stat()
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
-//
-//	// Устанавливаем заголовки
-//	w.Header().Set("Content-Disposition", "attachment; filename="+fileInfo.Name())
-//	w.Header().Set("Content-Type", "application/octet-stream")
-//	w.Header().Set("Content-Length", strconv.FormatInt(fileInfo.Size(), 10))
-//
-//	// Копируем содержимое файла в ResponseWriter
-//	_, err = io.Copy(w, file)
-//	if err != nil {
-//		http.Error(w, err.Error(), http.StatusInternalServerError)
-//		return
-//	}
-//}
